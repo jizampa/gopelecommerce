69ff4f0ab61de3632e0c4cf3422bd359e8d2ae3e
jizampa <jizampa@gmail.com>
2018-11-01 14:48:22 +0000

    changing column name for status

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Status do
+ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -10,6 +10,7 @@ ActiveAdmin.register Status do
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
-# end
-permit_params :name
+# 
+permit_params :province_id, :fname, :lname, :email, :street, :stNumber, :city
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_180019) do
+ActiveRecord::Schema.define(version: 2018_11_01_144559) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 2018_10_30_180019) do
   end
 
   create_table "statuses", force: :cascade do |t|
-    t.string "status"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
