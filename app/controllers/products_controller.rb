2afe18225b4dd4445548dd15a44cbd8e5ae1cce5
jizampa <jizampa@gmail.com>
2018-11-02 01:55:13 +0000

    add the nav, feature products are showing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,12 @@
 class ProductsController < ApplicationController
   def index
     @tag = Tag.all
+    @prod = Product.all
+    @fearured_product1 = Product.all.sample
+    @category = Category.all
   end
 
   def show
+     @product = Product.find(params[:id])
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :tag
   belongs_to :category
+  mount_uploader :image, ImageUploader
+
 end
