1953a9ea399ded02d061e4612ab6c13bc9f2e4dd
jizampa <jizampa@gmail.com>
2018-11-10 01:09:06 +0000

    working fine after fixing product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   
-  get 'pages/show'
+  resources :pages, only: [:show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :tags, only: [:show]
